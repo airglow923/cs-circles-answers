e9fd97abdd99fdb24f9a359cb1740c4c1c5bd94c
airglow923 <37762634+airglow923@users.noreply.github.com>
2018-03-25 18:23:11 +0800

    Update CS Circles 8 Watch the Pendulum.py

--- CS Circles 8 Watch the Pendulum.py
+++ CS Circles 8 Watch the Pendulum.py
@@ -1,5 +1,7 @@
 import math
-L = float(input())
+
+L = float(input())  # length can be either integer or float
 A = float(input())
-for T in range(0, 10):
-   print(L*math.cos(A*math.cos(T*math.sqrt(9.8/L)))-L*math.cos(A))
+
+for T in range(0, 10):  # loop for the function of X(T) which T equals to the values 0 to 9 
+    print(L*math.cos(A*math.cos(T*math.sqrt(9.8/L)))-L*math.cos(A))
