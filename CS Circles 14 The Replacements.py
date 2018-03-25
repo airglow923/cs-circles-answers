c3e76ee479a8b8dcf8180f421f24443e8cf98fcb
Hyundeok Park <37762634+airglow923@users.noreply.github.com>
2018-03-25 23:27:04 +0800

    Update CS Circles 14 The Replacements.py

--- CS Circles 14 The Replacements.py
+++ CS Circles 14 The Replacements.py
@@ -1,5 +1,5 @@
 def replace(list, X, Y):  # 'list' equals to the list of numbers
    while list.count(X) > 0:  # same as 'while X in list:'
-      a = list.index(X)  # locate where the 'X' value in the list 
+      a = list.index(X)  # locate where the 'X' value is in the list 
       list.pop(a)  # remove the 'x' value
       list.insert(a, Y)  # insert (or replace) the 'X' value into the 'Y' value
