5b51f3c692a191ac1e484231b9ce92fa233f3819
airglow923 <37762634+airglow923@users.noreply.github.com>
2018-03-25 18:27:10 +0800

    Update CS Circles 8 Centering Text.py

--- CS Circles 8 Centering Text.py
+++ CS Circles 8 Centering Text.py
@@ -1,11 +1,13 @@
 width = int(input())  # the length of text
 while True:
-   a = input()  # word for centering
-   L = len(a)  # length of the word
-   b = (width-L) // 2  # calculate how many periods are needed on both sides of the word (left side)
-   c = b  # this will be used for unbalanced length (right side)
-   if ((width-L) % 2) != 0:  # if periods needed on both sides are unequal
-      b = b + 1  # add one more period on the left side
-   if a == 'END':
-      break
-   print('.' * b + a + '.' * c)
+    a = input()  # word for centering
+    L = len(a)  # length of the word
+    b = (width-L) // 2  # calculate how many periods are needed on both sides of the word (left side)
+    c = b  # this will be used for unbalanced length (right side)
+    
+    if ((width-L) % 2) != 0:  # if periods needed on both sides are unequal
+        b = b + 1  # add one more period on the left side
+    if a == 'END':  # if the input is 'END'
+        break
+        
+    print('.' * b + a + '.' * c)
