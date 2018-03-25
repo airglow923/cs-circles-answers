e7327047484677125164ad3c5d5a09e90a6c2bcc
airglow923 <37762634+airglow923@users.noreply.github.com>
2018-03-25 18:44:47 +0800

    Update CS Circles 13 It's Natural.py

--- CS Circles 13 It's Natural.py
+++ CS Circles 13 It's Natural.py
@@ -1,5 +1,5 @@
 def naturalNumbers(x):
-   a = []
-   for i in range(1, x + 1):
-      a += [i]
-   return a
+    a = []  # create an empty list
+    for i in range(1, x + 1):
+        a += [i]
+    return a
