8893c3d7122628ca10a19f0e351402987250bf85
airglow923 <37762634+airglow923@users.noreply.github.com>
2018-03-25 17:33:43 +0800

    Update CS Circles 8 Python Adder.py

--- CS Circles 8 Python Adder.py
+++ CS Circles 8 Python Adder.py
@@ -1,6 +1,6 @@
 S = input()
 for position in range(0, len(S)):
    if S[position] == '+':  # seperate numbers with regard to '+'
-      str1 = int(S[0:position])  # turns str1 into number1
-      str2 = int(S[(position+1):len(S)])  # turns str2 into number2
+      str1 = int(S[0:position])  # turn str1 into number1
+      str2 = int(S[(position+1):len(S)])  # turn str2 into number2
       print(str1 + str2)
