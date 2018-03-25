8925619b281c2d2c093abfb868b9f02e4aecfbf8
airglow923 <37762634+airglow923@users.noreply.github.com>
2018-03-25 18:27:51 +0800

    Update CS Circles 7C Finding Factors.py

--- CS Circles 7C Finding Factors.py
+++ CS Circles 7C Finding Factors.py
@@ -1,5 +1,5 @@
 n = int(input())
 for a in range(1, n + 1):  # substitute all integers within the range of input
-   for b in range(1, n + 1):  # substitue all integers within the range of input
-      if a * b == n:
-         print (a, 'times', b, 'equals', n)
+    for b in range(1, n + 1):  # substitue all integers within the range of input
+        if a * b == n:
+            print (a, 'times', b, 'equals', n)
