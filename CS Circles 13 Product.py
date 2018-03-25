3342f27833a872a8c188aaab46edc3484a3c89d1
airglow923 <37762634+airglow923@users.noreply.github.com>
2018-03-25 18:35:16 +0800

    Update CS Circles 13 Product.py

--- CS Circles 13 Product.py
+++ CS Circles 13 Product.py
@@ -1,11 +1,5 @@
 def prod(L):
-   a = 1
-   for i in range(0, len(L)): # Computing product of the numbers in the list using range    
-      a *= L[i]
-   return a
-
-def prod(L):
-   a = 1
-   for i in L: # Computing product of the numbers in the list without using range
-      a *= i
-   return a
+    a = 1
+    for i in range(0, len(L)):  # same as 'for i in L:' as L is the list
+        a *= L[i]  # a = a * L[i]
+    return a
