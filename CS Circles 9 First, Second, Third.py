b557b2d2d9c2c7e681d95ca3a5a56db63827023f
airglow923 <37762634+airglow923@users.noreply.github.com>
2018-03-25 17:41:02 +0800

    Update CS Circles 9 First, Second, Third.py

--- CS Circles 9 First, Second, Third.py
+++ CS Circles 9 First, Second, Third.py
@@ -1,9 +1,9 @@
-a = int(input())
-if a / 1 == 1:
-   print(str(a) + 'st')
-elif a / 2 == 1:
-   print(str(a) + 'nd')
-elif a / 3 == 1:
-   print(str(a) + 'rd')
-else:
-   print(str(a) + 'th')
+a = int(input())  # convert input into integer as it is always a string
+if a / 1 == 1:  # if the input can be divided by 1
+   print(str(a) + 'st')  # 1st
+elif a / 2 == 1:  # if the input can be divided by 2
+   print(str(a) + 'nd')  # 2nd
+elif a / 3 == 1:  # if the input can be divided by 3
+   print(str(a) + 'rd')  # 3rd
+else:  # if not 1st, 2nd and 3rd
+   print(str(a) + 'th')  # (number)th
