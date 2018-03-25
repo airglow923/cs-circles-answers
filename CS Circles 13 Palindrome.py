d231bd02b78998b81f886335bb0eba97edbeb2fb
airglow923 <37762634+airglow923@users.noreply.github.com>
2018-03-25 18:40:48 +0800

    Update CS Circles 13 Palindrome.py

--- CS Circles 13 Palindrome.py
+++ CS Circles 13 Palindrome.py
@@ -1,5 +1,5 @@
 def isPalindrome(S):
-   if str(S) == str(S)[::-1]:
-      return True
-   else:
-      return False
+    if str(S) == str(S)[::-1]:  # if 'S' equals to the inverse of 'S'
+        return True
+    else:
+        return False
