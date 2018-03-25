907d046e2afcc610740d573ecdb5d294d51c05d6
airglow923 <37762634+airglow923@users.noreply.github.com>
2018-03-25 17:37:21 +0800

    Update CS Circles 8 Substring Counting.py

--- CS Circles 8 Substring Counting.py
+++ CS Circles 8 Substring Counting.py
@@ -1,7 +1,7 @@
-needle = input()   # defines first input string
-haystack = input()   # defines second input string
-times = 0   # defines number of times first input string is in second
-for a in range(0, len(haystack) - 1):   # loop to check strings within haystack
-    if haystack[a:len(needle) + a] == needle:   # checking for needles in haystack
-        times += 1   # counting the number of needeles
+needle = input()  # define the first input string
+haystack = input()  # define the second input string
+times = 0  # define how many times the needles will be counted
+for a in range(0, len(haystack) - 1):  # loop to check strings within haystack
+    if haystack[a:len(needle) + a] == needle:  # check for needles in haystack
+        times += 1  # count the number of needles
 print(times)
