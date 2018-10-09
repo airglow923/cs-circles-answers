ee31800b82818f04656c1c050cc36175c6a91063
Hyundeok Park <37762634+airglow923@users.noreply.github.com>
2018-10-09 23:10:09 +0800

    Update CS Circles 15A Sub-task 2.py

--- CS Circles 15A Sub-task 2.py
+++ CS Circles 15A Sub-task 2.py
@@ -1,6 +1,6 @@
 def findLine(prog, target):
     theList = []
-    for i in range(len(prog)):
-        theList = (prog[i].split())
-        if theList[0] == target:
+    for i in range(len(prog)):    # when prog = ['10 GOTO 20', '20 GOTO 30'], i in range(2)
+        theList = (prog[i].split())    # theList = ['10', 'GOTO', '20'] when i = 0 and ['20', 'GOTO', '30'] when i = 1
+        if theList[0] == target:    # if the first element of theList, which can either be 10 or 20, eqauls to target, return index
             return i
