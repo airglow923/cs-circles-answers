44a6cdbe49eb65ff60cd3a4f64df0b6f49489a52
Hyundeok Park <37762634+airglow923@users.noreply.github.com>
2018-10-09 23:19:47 +0800

    Update CS Circles 15A Sub-task 3.py

--- CS Circles 15A Sub-task 3.py
+++ CS Circles 15A Sub-task 3.py
@@ -1,16 +1,16 @@
 def execute(prog):
     location = 0
     while True:
-        if location == len(prog) - 1:
+        if location == len(prog) - 1:    # when index equals to the position of 'LINENUMBER END'
             return 'success'
         
-        theList = prog[location].split()
-        T = theList[2]
-        previous = theList[0]
-        location = findLine(prog, T)
-        theList = prog[location].split()
+        theList = prog[location].split()    # ['10 GOTO 20'] -> theList = ['10', 'GOTO' '20']
+        T = theList[2]    # when theList = ['LINENUMBER GOTO TARGET'], T = TARGET
+        previous = theList[0]    # previous = LINENUMBER
+        location = findLine(prog, T)    # when prog[i] = ['TARGET GOTO ANOTHER TARGET'], location = index of prog[i]
+        theList = prog[location].split()    # New line means theList = ['TARGET', 'GOTO', 'ANOTHER TARGET']
         
-        if len(theList) == 3:    # 'LINENUMBER END' has length of 2
+        if len(theList) == 3:    # 'LINENUMBER END' has length of 2; when theList does not have END
             present = theList[2]
-            if previous == present:
+            if previous == present:    # ['10', 'GOTO', '20'] ['20', 'GOTO', '10']
                 return 'infinite loop'
