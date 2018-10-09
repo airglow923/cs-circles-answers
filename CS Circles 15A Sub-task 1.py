479aeab5fbfed41e835ea54452ed3f7d1efb9e6d
Hyundeok Park <37762634+airglow923@users.noreply.github.com>
2018-10-09 22:56:11 +0800

    Delete CS Circles 15A Sub-task 1.py

--- CS Circles 15A Sub-task 1.py
+++
@@ -1,9 +0,0 @@
-def getBASIC():
-    theList = []
-    nothing = ''
-    while True:
-        userInput = input()
-        statement = userInput.split()
-        theList.append(userInput)
-        if statement[1] == 'END':
-           return theList
