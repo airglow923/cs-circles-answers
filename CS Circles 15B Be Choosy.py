80ecbc5da23c6332b198112fe541ff73c1958728
Hyundeok Park <37762634+airglow923@users.noreply.github.com>
2018-10-12 00:00:21 +0800

    Add files via upload

---
+++ CS Circles 15B Be Choosy.py
@@ -0,0 +1,14 @@
+def choose(n, k):
+    foo = 1
+    for i in range(n):
+        foo *= n - i
+
+    bar = 1
+    for i in range(k):
+        bar *= k - i
+
+    baz = 1
+    for i in range(n - k):
+        baz *= (n - k) - i
+
+    return foo/(bar*baz)
